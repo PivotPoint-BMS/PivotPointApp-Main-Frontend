17fc4a802874c6ad562004bd4c113a1b6514ff8b
Yaniss Guendouzi <guendouziyaniss@gmail.com>
2023-01-22 15:17:24 +0100

    added storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,36 @@
+// .storybook/preview.js
+
+import '../styles/globals.css'
+import './font.css'
+import * as NextImage from 'next/image'
+import { themes } from '@storybook/theming'
+
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+})
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  previewTabs: {
+    'storybook/docs/panel': { index: -1 },
+  },
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark },
+    // Override the default light theme
+    light: { ...themes.normal, appBg: 'white' },
+    darkClass: 'dark',
+    lightClass: 'light',
+    classTarget: 'html',
+    stylePreview: true,
+  },
+}
---
+++ stories/Button.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Meta, Story } from '@storybook/react'
+import Button, { ButtonProps } from '../components/Button'
+
+const meta: Meta = {
+  title: 'INPUT/Button',
+  component: Button,
+  argTypes: {
+    children: {
+      control: {
+        type: 'Button',
+      },
+    },
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+}
+
+export default meta
+
+const Template: Story<ButtonProps> = (args) => <Button {...args}>Button</Button>
+
+export const Contained = Template.bind({})
+export const Outlined = Template.bind({})
+
+Contained.args = {
+  variant: 'contained',
+}
+Outlined.args = {
+  variant: 'outlined',
+}
