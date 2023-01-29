84e6fec40dbef8b1036ff495b07d12762b3a2593
Yaniss Guendouzi <guendouziyaniss@gmail.com>
2023-01-29 13:06:41 +0100

    added sidebar animation

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,12 +1,23 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
+import { useRouter } from 'next/router'
+// redux
+import { useAppDispatch } from 'store/hooks'
+import { close } from 'store/sideBarSlice'
 // hooks
 import useResponsive from 'hooks/useResponsive'
 import Header from './Header'
+// components
 import SidebarDesktop from './SidebarDesktop'
 import SidebarMobile from './SidebarMobile'
 
 function Layout({ children }: { children: React.ReactNode | React.ReactNode[] }) {
   const isDesktop = useResponsive('lg', 'up')
+  const router = useRouter()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(close())
+  }, [router.pathname])
 
   return (
     <div className='flex h-screen w-full'>
