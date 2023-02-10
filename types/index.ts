06f118263665345a182254d64846ed3354792fbf
Yaniss Guendouzi <guendouziyaniss@gmail.com>
2023-02-10 11:58:35 +0100

    minor

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,3 @@
-export { default as ResetPasswordInput } from './ResetPasswordInput'
 export type { default as ResetPasswordInput } from './ResetPasswordInput'
 export type { default as RegisterInput } from './RegisterInput'
 export type { default as IGenericResponse } from './IGenericResponse'
