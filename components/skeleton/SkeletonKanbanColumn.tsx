54599701755267f610fbe53bb6bccc31d85b09fd
Yaniss Guendouzi <guendouziyaniss@gmail.com>
2023-05-06 13:44:13 +0100

    to remove

---
+++ components/skeleton/SkeletonKanbanColumn.tsx
@@ -0,0 +1,17 @@
+import { Card } from '../Card'
+
+export default function SkeletonKanbanColumn() {
+  return (
+    <div className='grid grid-cols-4 gap-1'>
+      {[...Array(3)].map((_, index) => (
+        <Card variant='outlined' key={index} className='w-80 p-1'>
+          <div className='flex flex-col items-center justify-center gap-2'>
+            <div className='h-full w-full rounded-lg bg-gray-600 pt-6' />
+            {index === 0 && <div className='h-full w-full rounded-lg bg-gray-600 pt-6' />}
+            {index !== 2 && <div className='h-full w-full rounded-lg bg-gray-600 pt-6' />}
+          </div>
+        </Card>
+      ))}
+    </div>
+  )
+}
--- sections/dashboard/crm/sales-pipeline/DealsKanban.tsx
+++ sections/dashboard/crm/sales-pipeline/DealsKanban.tsx
@@ -1,158 +1,53 @@
 import React, { useState } from 'react'
-// dnd
-import {
-  useSensors,
-  useSensor,
-  PointerSensor,
-  KeyboardSensor,
-  DndContext,
-  closestCorners,
-  DragEndEvent,
-  DragStartEvent,
-  DragOverEvent,
-  DragOverlay,
-  DropAnimation,
-  defaultDropAnimation,
-} from '@dnd-kit/core'
-import { sortableKeyboardCoordinates, arrayMove } from '@dnd-kit/sortable'
-// types
-import { Deal } from 'types'
-// utils
-import { getTaskById } from './kanbanItemUtils'
-import { KanbanColumsType, findKanbanColumContainer, initializeBoard } from './board'
-// components
-import KanbanColumn from './KanbanColumn'
-import DealItem from './DealItem'
+import { DndContext, DragEndEvent, DragOverlay, DragStartEvent } from '@dnd-kit/core'
+import { SortableContext } from '@dnd-kit/sortable'
+import Column from './Column'
 
-const INITIAL_TASKS: Deal[] = [
-  {
-    id: '1',
-    title: 'Title 2',
-    description: 'Desc 2',
-    status: 'qualification',
-  },
-  {
-    id: '2',
-    title: 'Title 3',
-    description: 'Desc 3',
-    status: 'qualification',
-  },
-  {
-    id: '3',
-    title: 'Title 4',
-    description: 'Desc 4',
-    status: 'done',
-  },
-  {
-    id: '4',
-    title: 'Title 4',
-    description: 'Desc 4',
-    status: 'testing',
-  },
-]
+const items = ['1', '2', '3', '4']
+const subItems: { [key: string]: string[] } = {
+  1: ['A1', 'B1', 'C1'],
+  2: ['A2', 'B2', 'C2'],
+  3: ['A3', 'B3', 'C3'],
+}
+export default function DealsKanban() {
+  const [activeId, setActiveId] = useState<string | null>(null)
+  const [activeType, setActiveType] = useState<string | null>(null)
 
-const DealsKanban = () => {
-  const deals = INITIAL_TASKS
-  const initialBoardSections = initializeBoard(INITIAL_TASKS)
-  const [kanbanColumns, setKanbanColumns] = useState<KanbanColumsType>(initialBoardSections)
-
-  const [activeTaskId, setActiveTaskId] = useState<null | string>(null)
-
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  )
-
-  const handleDragStart = ({ active }: DragStartEvent) => {
-    setActiveTaskId(active.id as string)
+  function handleDragStart(event: DragStartEvent) {
+    setActiveId(event.active.id.toString())
+    setActiveType(event.active.data.current?.type)
+    // console.log(event.active.data.current?.type)
   }
 
-  const handleDragOver = ({ active, over }: DragOverEvent) => {
-    // Find the containers
-    const activeContainer = findKanbanColumContainer(kanbanColumns, active.id as string)
-    const overContainer = findKanbanColumContainer(kanbanColumns, over?.id as string)
-
-    if (!activeContainer || !overContainer || activeContainer === overContainer) {
-      return
-    }
-
-    setKanbanColumns((kanbanColumn) => {
-      const activeItems = kanbanColumn[activeContainer]
-      const overItems = kanbanColumn[overContainer]
-
-      // Find the indexes for the items
-      const activeIndex = activeItems.findIndex((item) => item.id === active.id)
-      const overIndex = overItems.findIndex((item) => item.id !== over?.id)
-
-      return {
-        ...kanbanColumn,
-        [activeContainer]: [
-          ...kanbanColumn[activeContainer].filter((item) => item.id !== active.id),
-        ],
-        [overContainer]: [
-          ...kanbanColumn[overContainer].slice(0, overIndex),
-          kanbanColumns[activeContainer][activeIndex],
-          ...kanbanColumn[overContainer].slice(overIndex, kanbanColumn[overContainer].length),
-        ],
-      }
-    })
+  function handleDragEnd(event: DragEndEvent) {
+    setActiveId(null)
+    console.log(event.active.data.current?.type)
   }
-
-  const handleDragEnd = ({ active, over }: DragEndEvent) => {
-    const activeContainer = findKanbanColumContainer(kanbanColumns, active.id as string)
-    const overContainer = findKanbanColumContainer(kanbanColumns, over?.id as string)
-
-    if (!activeContainer || !overContainer || activeContainer !== overContainer) {
-      return
-    }
-
-    const activeIndex = kanbanColumns[activeContainer].findIndex((deal) => deal.id === active.id)
-    const overIndex = kanbanColumns[overContainer].findIndex((deal) => deal.id === over?.id)
-
-    if (activeIndex !== overIndex) {
-      setKanbanColumns((kanbanColumn) => ({
-        ...kanbanColumn,
-        [overContainer]: arrayMove(kanbanColumn[overContainer], activeIndex, overIndex),
-      }))
-    }
-
-    setActiveTaskId(null)
-  }
-
-  const dropAnimation: DropAnimation = {
-    ...defaultDropAnimation,
-  }
-
-  const deal = activeTaskId ? getTaskById(deals, activeTaskId) : null
 
   return (
-    <div className='w-auto overflow-hidden'>
+    <div>
       <DndContext
-        sensors={sensors}
-        collisionDetection={closestCorners}
         onDragStart={handleDragStart}
-        onDragOver={handleDragOver}
         onDragEnd={handleDragEnd}
+        // modifiers={[restrictToHorizontalAxis]}
       >
-        <div className='scrollbar-none flex h-full flex-row items-start gap-6 overflow-x-scroll'>
-          {Object.keys(kanbanColumns).map((kanbanColumnKey) => (
-            <div key={kanbanColumnKey}>
-              <KanbanColumn
-                id={kanbanColumnKey}
-                title={kanbanColumnKey}
-                deals={kanbanColumns[kanbanColumnKey]}
-              />
-            </div>
-          ))}
-          <DragOverlay dropAnimation={dropAnimation}>
-            {deal ? <DealItem deal={deal} /> : null}
-          </DragOverlay>
+        <div className='scrollbar-none flex gap-4 overflow-scroll'>
+          <SortableContext items={items}>
+            {items.map((id: string) => (
+              <Column key={id} id={id} items={subItems[id]} />
+            ))}
+          </SortableContext>
         </div>
+        <DragOverlay>
+          {activeId ? (
+            <>
+              {activeType === 'card' ? null : ( // <Item id={activeId} />
+                <Column id={activeId} isDragOverlay items={subItems[activeId]} />
+              )}
+            </>
+          ) : null}
+        </DragOverlay>
       </DndContext>
     </div>
   )
 }
-
-export default DealsKanban
